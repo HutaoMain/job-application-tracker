dd69731f583a696569b3cc50da560a9f063a8778
Ali Alcantara <ali.alcantara@novare.com.hk>
2024-02-18 19:37:25 +0800

    put the card into separate component initial design

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { JobInterface } from '../interface/job-interface';
 import { CommonModule } from '@angular/common';
-import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [CommonModule, CdkDrag],
+  imports: [CommonModule],
   templateUrl: './job-card.component.html',
   styleUrl: './job-card.component.css',
 })
